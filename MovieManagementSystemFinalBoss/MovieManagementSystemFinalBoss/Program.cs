fcb298f6805d7b4bff02ebbea0012622e6aaba62
frankikwuegbu <99530481+frankikwuegbu@users.noreply.github.com>
2025-11-20 16:42:45 +0100

    initial commit

---
+++ MovieManagementSystemFinalBoss/MovieManagementSystemFinalBoss/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagementSystemFinalBoss
+{
+    class Movies
+    {
+        //fieldss
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public bool IsRented { get; set; }
+
+        //constructor
+        public Movies(string title, string genre, int year, bool isRented = false)
+        {
+            Title = title;
+            Genre = genre;
+            Year = year;
+            IsRented = isRented;
+        }
+        public virtual void MovieDetails()
+        {
+            Console.Write("movie title: ");
+            string title = Console.ReadLine();
+            Title = title;
+            Console.Write("movie genre: ");
+            string genre = Console.ReadLine();
+            Genre = genre;
+            Console.Write("release year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Year = year;
+        }
+        public Movies()
+        {
+            MovieDetails();
+        }
+
+        //output
+        public override string ToString()
+        {
+            return $"movie title: {Title}\nmovie genre: {Genre}\nmovie Year: {Year}";
+        }
+    }
+
+    class Series: Movies
+    {
+        public string LeadAct { get; set; }
+
+        public override void MovieDetails()
+        {
+            Console.Write("series title: ");
+            string seriesTitle = Console.ReadLine();
+            Title = seriesTitle;
+            Console.Write("lead act: ");
+            string mainCharacter = Console.ReadLine();
+            LeadAct = mainCharacter;
+        }
+
+        public Series()
+        {
+            MovieDetails();
+        }
+    }
+
+    abstract class Member
+    {
+        public string Name { get; set; }
+
+        public Member(string name) { Name = name; }
+
+        public List<Movies> movieList = new List<Movies>();
+        public abstract Movies AddMovie();
+        
+        public abstract void ShowMovies();
+        public abstract void LoginMessage();
+    }
+
+    class Guest : Member
+    {
+        public Guest(string name) : base(name) { }
+
+        public override Movies AddMovie()
+        {
+            Movies movie = new Movies();
+            movieList.Add(movie);
+            Console.WriteLine(movie);
+
+            return movie;
+        }
+
+        public override void ShowMovies()
+        {
+            foreach (var item in movieList)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
+
+        public override void LoginMessage()
+        {
+            Console.WriteLine($"{Name} logged in as guest!");
+        }
+    }
+
+    class User : Member
+    {
+        public User(string name) : base(name) { }
+        public override Movies AddMovie()
+        {
+            Movies movie = new Movies();
+            movieList.Add(movie);
+            Console.WriteLine("movie added!\n");
+
+            return movie;
+        }
+
+        public override void ShowMovies()
+        {
+            foreach (var item in movieList)
+            {
+                Console.WriteLine(item.Title);
+            }
+        }
+
+        public void RentMovie()
+        {
+            bool matchingTitle = false;
+            Console.Write("what movie do you want to rent: ");
+            string movieTitle = Console.ReadLine();
+            foreach (var item in movieList)
+            {
+                if (item.Title == movieTitle && item.IsRented == false)
+                {
+                    Console.WriteLine($"{movieTitle} is available to rent!");
+                    item.IsRented = true;
+                    matchingTitle = true;
+                }
+                else if (item.Title == movieTitle && item.IsRented == true)
+                {
+                    Console.WriteLine($"{movieTitle} has already been rented by you!");
+                    matchingTitle = true;
+                }
+            }
+            if (!matchingTitle)
+            {
+                Console.WriteLine($"{movieTitle} NOT FOUND!");
+            }
+        }
+        public override void LoginMessage()
+        {
+            Console.WriteLine($"welcome, {Name}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic user;
+
+            Console.Write("1. user\n2. guest\nselect login type: ");
+            int login = Convert.ToInt32( Console.ReadLine() );
+
+            if (login == 1) { user = NewUser(); } else { user = NewGuest(); }
+
+            Console.Write("1. add movie \n2 show movies\n3 rent a movie" +
+                          "\nselect action: ");
+            int press = Convert.ToInt32(Console.ReadLine());
+
+            while (press != 0)
+            {
+                if (press == 1) { user.AddMovie(); }
+                else if (press == 2) { user.ShowMovies(); }
+                else if (press == 3) { user.RentMovie(); }
+
+                Console.Write("select operation: ");
+                press = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.ReadLine();
+        }
+
+        static User NewUser()
+        {
+            Console.Write("username: ");
+            string userName = Console.ReadLine();
+
+            var user = new User(userName);
+            user.LoginMessage();
+            //Console.WriteLine(user);
+            return user;
+        }
+
+        static Guest NewGuest()
+        {
+            Console.Write("guest name: ");
+            string guestName = Console.ReadLine();
+
+            var guest = new Guest(guestName);
+            guest.LoginMessage();
+            //Console.WriteLine(user);
+            return guest;
+        }
+    }
+}
---
+++ tryingthings/tryingthings/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryingthings
+{
+    //inheritance and polymorphism
+    class People
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public People(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void SayMyName()
+        {
+            Console.WriteLine($"my name is {Name}");
+        }
+
+        public void HowOldAreYou()
+        {
+            Console.WriteLine($"i am {Age} years old");
+        }
+
+         //the contents of a virtual class can be overwritten in a child class
+        //this is called polymorphism
+        public virtual void Communicate()
+        {
+            Console.WriteLine("i can speak!");
+        }
+    }
+
+    class Registration : People
+    {
+        public int PhoneNumber { get; set; }
+        public string Language { get; set; }
+        public Registration (string name, int age, int phoneNumber, string language) : base(name, age)
+        {
+            PhoneNumber = phoneNumber;
+            Language = language;
+        }
+
+        public void CallMe()
+        {
+            Console.WriteLine($"here is my number: {PhoneNumber}");
+        }
+
+        //overriding method from parent class
+        public override void Communicate()
+        {
+            Console.WriteLine($"i speak {Language}");
+        }
+    }
+
+     //abstract classes cannot have object instances
+    //its "contents" can only be accessed from an inheriting class
+    abstract class Animals
+    {
+        public string Name { get; set; }
+
+        //the body of an abstract method is defined in the inheriting class
+        public abstract void Sound();
+
+        //abstract classes can have regular methods
+        public void Sleep()
+        {
+            Console.WriteLine("zzzzzzz");
+        }
+    }
+
+    class Dog: Animals
+    {
+        //overriding abstract method
+        public override void Sound()
+        {
+            Console.WriteLine("bark!");
+        }
+    }
+
+    //Interface
+    interface ICar
+    {
+        string Type { get; set; }
+        void Model();
+    }
+
+    interface ICarinterior
+    {
+        void CarSeats();
+    }
+
+    //a class can "inherit" an Interface
+    //the inheriting class MUST have all properties and methods from the Interface
+    //one class can "inherit" multiple interfaces
+    class Toyota : ICar, ICarinterior
+    {
+        public string Type { get; set; }
+        public void Model()
+        {
+            Console.WriteLine($"Model: {Type}");
+        }
+
+        public void CarSeats()
+        {
+            Console.WriteLine("four seater vehicle");
+        }
+
+        //inheriting class can also have its own unique methods (and/or properties)
+        public void NitroSpeed()
+        {
+            Console.WriteLine("zoooom!");
+        }
+    }
+
+    //enum
+    enum Login { AsUser, AsGuest }
+
+    //enum in a class
+    class Pc
+    {
+        public enum Brands
+        {
+            dell, hp, macbook
+        }
+
+        public Brands Type { get; set; }
+
+        public void Something() { }
+
+        public void Anotherthing(int random)
+        {
+            if (random == (int)Brands.dell)
+            {
+                Console.WriteLine("only dell laptops can do this");
+            }
+            else
+            {
+                Console.WriteLine("you are not permitted to do this");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Registration randomPeople = new Registration(phoneNumber:234, name: "trevor", age:60, language: "French");
+            randomPeople.SayMyName();
+            randomPeople.HowOldAreYou();
+            randomPeople.CallMe();
+            randomPeople.Communicate();
+
+            People randomPeople2 = new People("patrick", 30);
+            randomPeople2.SayMyName();
+
+            Dog bullDog = new Dog();
+            bullDog.Name = "bingo";
+            bullDog.Sound();
+            bullDog.Sleep();
+
+            ICar car = new Toyota();
+            car.Type = "Camry";
+            //car.NitroBoost();   //car cannot access NitroSpeed() because of its type
+
+            Toyota car2 = new Toyota();
+            car2.NitroSpeed();      //NitroSpeed() is specific to objects of class-type Toyota
+            car2.CarSeats();
+            Console.WriteLine(car.Type);
+
+            //enum => line 123
+            int number = 1;
+            if (number == (int)Login.AsUser)
+            {
+                Console.WriteLine("logged in as user");
+            }
+            else
+            {
+                Console.WriteLine("logged in as guest");
+            }
+
+            Pc newPc = new Pc();
+            newPc.Type = Pc.Brands.dell;
+            Console.WriteLine(newPc.Type);  //output: dell
+
+            Console.Write("number: ");
+            int result = Convert.ToInt32(Console.ReadLine());
+
+            newPc.Anotherthing(result);
+
+            Console.ReadLine();
+        }
+    }
+}
